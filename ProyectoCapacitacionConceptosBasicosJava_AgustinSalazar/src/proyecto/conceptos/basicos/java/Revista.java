30f9a875e6046cdec7100b76fccaeb0fe66c4c94
Agustín Salazar <agustin.salazar@utp.edu.co>
2022-04-22 11:31:54 -0500

    eliminado del main

--- ProyectoCapacitacionConceptosBasicosJava_AgustinSalazar/src/proyecto/conceptos/basicos/java/Revista.java
+++
@@ -1,8 +0,0 @@
-package proyecto.conceptos.basicos.java;
-
-public class Revista extends Producto{
-	
-	public Revista(String fecha, String nombre, String autor, String editorial, float precio, int cantidad) {
-        super(fecha, "Revista", nombre, autor, editorial, precio, cantidad);
-    }
-}

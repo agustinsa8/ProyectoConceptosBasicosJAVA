10c75798f13990572ebecd71b50c2446909c5513
Agustín Salazar <agustin.salazar@utp.edu.co>
2022-04-22 11:25:53 -0500

    prueba eliminada

--- ProyectoCapacitacionConceptosBasicosJava_AgustinSalazar/src/proyecto/conceptos/basicos/java/Articulo.java
+++
@@ -1,9 +0,0 @@
-package proyecto.conceptos.basicos.java;
-
-public class Articulo extends Producto {
-	
-	public Articulo(String fecha, String nombre, String autor, String editorial, float precio, int cantidad) {
-        super(fecha, "Articulo", nombre, autor, editorial, precio, cantidad);
-    }
-
-}
